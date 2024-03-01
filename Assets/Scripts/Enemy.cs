4e852398d21b5e0574f658fb4b169f5c14aabf32
mdengra9 <mireia.den.esp@gmail.com>
2024-03-01 09:00:17 +0100

    dfdgd

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public enum State
+    {
+        Patrolling,
+        Chasing,
+        Attacking
+    }
+
+    public State currentState;
+    private NavMeshAgent agent;
+    public Transform player;
+
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private float detectionRange = 15;
+    [SerializeField] private float attackingRange = 5;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomPoint();
+        currentState = State.Patrolling;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Patrolling:
+                Patrol;
+            break;
+
+            case State.Chasing:
+                Chase;
+            break;
+
+            case State.Attacking:
+                Attack;
+            break;
+        }
+    }
+
+    void Patrol()
+    {
+        if(IsInRange (detectionRange) == true)
+        {
+            currentState = State.Chasing;
+        }
+
+        if(agent.reimainingDistance < 0.5f)
+        {
+            SetRandomPoint();
+        }
+    }
+
+    void Chase()
+    {
+        if(IsInRange (detectionRange) == false)
+        {
+            SetRandomPoint();
+            currentState = State.Patrolling;
+        }
+
+        if(IsInRange (attackingRangeRange) == true)
+        {
+            currentState = State.Attacking;
+        }
+
+        agent.destination = player.position;
+    }
+
+    void Attack()
+    {
+        Debud.Log("Atacando");
+        currentState = State.Chasing;
+    }
+
+    void SetRandomPoint()
+    {
+        agent.destination = patrolPoints[Random.Range (0, 4)].position;
+    }
+
+    bool IsInRange (float Range)
+    {
+        if(Vector3.Distance (transform.position, player.position) < range)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
